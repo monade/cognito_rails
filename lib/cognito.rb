500f74581498b61dc30a8d638d75e2a20c965e93
Andrea Massioli <andream@monade.io>
2022-03-21 15:11:54 +0100

    WIP

--- lib/cognito.rb
+++ lib/cognito.rb
@@ -133,22 +133,20 @@ class CognitoUser
   end
 end
 
-# module Cognito
-#   extend ActiveSupport::Concern
+module Cognito
+  extend ActiveSupport::Concern
 
-#   included do
-#     before_save do puts "FROM GEM"
-#   end
+  # included do
+  #   before_save do puts "FROM GEM"
+  # end
 
-#   module Initializer
-#     def as_cognito_user
-#       puts "INITIALIZER"
-#       send :include, Cognito
-#     end
-#   end
+  module Initializer
+    def as_cognito_user
+			send :include, Cognito
+		end
+	end
 
-#   module ClassMethods
-#   end
-
-# end
-# ActiveRecord::Base.send :extend, Cognito::Initializer
+  # module ClassMethods
+  # end
+end
+ActiveRecord::Base.send :extend, Cognito::Initializer
