aac9e5db91408636221215d66c864b0e358bc8d6
Andrea Massioli <andream@monade.io>
2022-03-24 09:49:15 +0100

    WIP

--- lib/cognito_rails.rb
+++ lib/cognito_rails.rb
@@ -6,10 +6,28 @@ require 'cognito_rails/user'
 
 module CognitoRails
   extend ActiveSupport::Concern
+
+  module Initializer
+    def as_cognito_user
+      send :include, CognitoRails
+    end
+  end
+
+  included do
+    before_create do
+      self.init_cognito_user
+    end
+  
+    after_destroy do
+      self.destroy_cognito_user
+    end
+  end
   
   def cognito_user
     @cognito_user ||= User.find(external_id)
   end
+
+  protected
 
   def init_cognito_user
     return if external_id.present?
@@ -22,11 +40,5 @@ module CognitoRails
   def destroy_cognito_user
     cognito_user&.destroy!
   end
-
-  module Initializer
-    def as_cognito_user
-			send :include, CognitoRails
-		end
-	end
 end
 ActiveRecord::Base.send :extend, CognitoRails::Initializer
